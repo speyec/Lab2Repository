b85debac53dea9391b387ab6bd579e28d0c0f651
Michael <ouatum44@gmail.com>
2025-04-15 12:04:28 -0400

    added files

---
+++ CurrencyConverters/src/CurrencyConverter.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class CurrencyConverter {
+
+	public static void main(String[] args) {
+        System.out.println("Hello CC");
+    }
+
+    public BigDecimal convert(BigDecimal inputAmount, BigDecimal conversionRate) {
+        // TODO Auto-generated method stub
+        return inputAmount.multiply(conversionRate).setScale(2, RoundingMode.HALF_UP);
+    }
+
+}
