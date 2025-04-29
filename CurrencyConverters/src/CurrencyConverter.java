de693dcb55c0155b7d7a809b45c3d14f10dea42d
Michael <ouatum44@gmail.com>
2025-04-29 13:09:11 -0400

    Added test cases and isValid()

--- CurrencyConverters/src/CurrencyConverter.java
+++ CurrencyConverters/src/CurrencyConverter.java
@@ -13,5 +13,12 @@ public class CurrencyConverter {
         // TODO Auto-generated method stub
         return inputAmount.multiply(conversionRate).setScale(2, RoundingMode.HALF_UP);
     }
+    public boolean isValid(BigDecimal conversionRate) {
+        // TODO Auto-generated method stub
+    	//modify this
+        return  conversionRate.compareTo(BigDecimal.ZERO) > 0 
+                && conversionRate.compareTo(new BigDecimal("100000")) <= 0
+                && conversionRate != null;
+    }
 
 }
--- CurrencyConverters/src/CurrencyConverterTest.java
+++ CurrencyConverters/src/CurrencyConverterTest.java
@@ -24,19 +24,107 @@ public class CurrencyConverterTest {
         assertEquals(expectedResult, actualResult); // order is by convention
     }
 
-//    @Test
-//    public void testConvertRounding() {
-//        BigDecimal conversionRate = BigDecimal.valueOf(0.055);
-//        BigDecimal inputAmount = BigDecimal.valueOf(99.99);
-//        BigDecimal expectedResult = BigDecimal.valueOf(5.50).setScale(CurrencyConverter.DECIMAL_DIGITS);
-//        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
-//        assertEquals(expectedResult, actualResult);
-//    }
-//
-//    @Test
-//    public void testIsValid() {
-//        BigDecimal conversionRate = BigDecimal.valueOf(2.70);
-//        assertTrue(converter.isValid(conversionRate));
-//    }
+    @Test
+    public void testConvertRounding() {
+        BigDecimal conversionRate = BigDecimal.valueOf(0.055);
+        BigDecimal inputAmount = BigDecimal.valueOf(99.99);
+        BigDecimal expectedResult = BigDecimal.valueOf(5.50).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertEquals(expectedResult, actualResult);
+    }
 
+    @Test
+    public void testIsValid() {
+        BigDecimal conversionRate = BigDecimal.valueOf(2.70);
+        assertTrue(converter.isValid(conversionRate));
+    }
+    
+    @Test
+    public void testIsValidPositive() {
+    	// should not work --> it fails because it is too large
+    	BigDecimal conversionRate = BigDecimal.valueOf(100001);
+    	assertTrue(converter.isValid(conversionRate));
+    }
+    
+    @Test
+    public void testIsValidNegative() {
+    	//should fail because value of conversionRate is negative
+    	BigDecimal conversionRate = BigDecimal.valueOf(-100);
+    	assertTrue(converter.isValid(conversionRate));
+    }
+    
+    @Test
+    public void testIsValidnegativeResult() {
+    	// should fail because conversion rate is negative
+    	BigDecimal conversionRate = BigDecimal.valueOf(-2.70);
+        BigDecimal inputAmount = BigDecimal.valueOf(99.99);
+        BigDecimal expectedResult = BigDecimal.valueOf(-269.97).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertTrue(converter.isValid(conversionRate));
+        assertEquals(expectedResult, actualResult); 
+    }
+    
+    @Test
+    public void testIsValid100000() {
+    	//should pass
+    	BigDecimal conversionRate = BigDecimal.valueOf(100000);
+    	assertTrue(converter.isValid(conversionRate));
+    }
+    
+    @Test
+    public void testIsValid0() {
+    	//should fail
+    	BigDecimal conversionRate = BigDecimal.valueOf(0.00);
+    	assertTrue(converter.isValid(conversionRate));
+
+    }
+    
+    @Test
+    public void testConvertWrongEquals() {
+    	//should fail
+    	BigDecimal conversionRate = BigDecimal.valueOf(2.70);
+        BigDecimal inputAmount = BigDecimal.valueOf(99.99);
+        BigDecimal expectedResult = BigDecimal.valueOf(269.96).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertTrue(converter.isValid(conversionRate));
+        assertEquals(expectedResult, actualResult); 
+    	
+    }
+    @Test
+    public void testIsValidLarge() {
+    	// should pass
+    	BigDecimal conversionRate = BigDecimal.valueOf(99999);
+        BigDecimal inputAmount = BigDecimal.valueOf(99.99);
+        BigDecimal expectedResult = BigDecimal.valueOf(9998900.01).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertTrue(converter.isValid(conversionRate));
+        assertEquals(expectedResult, actualResult); 
+    }
+    @Test
+    public void testIsValidTooLarge() {
+    	// should fail because conversion rate is larger than 100000
+    	BigDecimal conversionRate = BigDecimal.valueOf(100000.01);
+        BigDecimal inputAmount = BigDecimal.valueOf(99.99);
+        BigDecimal expectedResult = BigDecimal.valueOf(9999001.00).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertTrue(converter.isValid(conversionRate));
+        assertEquals(expectedResult, actualResult); 
+    }
+    @Test
+    public void testConvertRoundingBig() {
+        BigDecimal conversionRate = BigDecimal.valueOf(0.055);
+        BigDecimal inputAmount = BigDecimal.valueOf(99999.99);
+        BigDecimal expectedResult = BigDecimal.valueOf(5500.00).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testConvertRoundingThousandths() {
+    	//fails because conert() only rounds the first 3 decimal places to 2
+        BigDecimal conversionRate = BigDecimal.valueOf(0.49585284);
+        BigDecimal inputAmount = BigDecimal.valueOf(11092.01);
+        BigDecimal expectedResult = BigDecimal.valueOf(5500.01).setScale(CurrencyConverter.DECIMAL_DIGITS);
+        BigDecimal actualResult = converter.convert(inputAmount, conversionRate);
+        assertEquals(expectedResult, actualResult);
+    }
 }
